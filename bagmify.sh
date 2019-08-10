3f617cac21a0ef39ae780175554c4297f14f3c6f
Dom Rodriguez <shymega@shymega.org.uk>
2019-08-10 20:47:04 +0100

    Add `set -eu` and fix formatting
    `set -eu` is a shell option, which a) halts the script if a non-zero
    return code is returned and b) halts the script if a value is unbound.
    
    I also fixed the formatting of the `update()` function declaration.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- bagmify.sh
+++ bagmify.sh
@@ -18,12 +18,13 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #######################################################################
 
+set -eu
+
 WATCH_DIR="$1"
 GIT_PATH="$2"
 REMOTE_REPO_NAME="bagmify"
 
-update()
-{
+update() {
     # commit everything
     git add --all
     git commit -m "$(date) automatic commit."
